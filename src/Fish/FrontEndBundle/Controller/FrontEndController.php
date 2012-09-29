1fc417745ac69c141b6e25281276f093707a5b5e
Alexander Fisher <mail@alexander-fisher.com>
2012-09-29 00:32:15 -0600

    Finished contact page. Fixed some templating.

--- src/Fish/FrontEndBundle/Controller/FrontEndController.php
+++ src/Fish/FrontEndBundle/Controller/FrontEndController.php
@@ -5,6 +5,7 @@ namespace Fish\FrontEndBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class FrontEndController extends Controller
 {
@@ -21,13 +22,61 @@ class FrontEndController extends Controller
      * @Route("/contact", name="fish_frontend_contact")
      * @Template()
      */
-    public function contactAction()
+    public function contactAction(Request $request)
     {
-        return array();
+        $form = $this->createFormBuilder()
+                ->add('email', 'email', array('label' => 'Your Email'))
+                ->add('subject')
+                ->add('message', 'textarea')
+                ->add('antibot', 'choice', array(
+                    'label' => 'Antibot',
+                    'choices' => array(
+                        'red' => 'The sky is red', 
+                        'purple' => 'The sky is purple',
+                        'blue' => 'The sky is blue',
+                        'yellow' => 'The skiy is yellow'
+                    )
+                    
+                ))
+                ->getForm();
+        
+        $errors = null;
+        
+        if ($request->getMethod() === "POST")
+        {
+            $form->bindRequest($request);
+            
+            if ($form->isValid())
+            {
+                $data = $form->getData();
+                
+                if ($data['antibot'] !== 'blue')
+                {
+                    $errors = "The sky is not " . $data['antibot'] . ". Are you sure you aren't a robot?";
+                }
+                else
+                {
+                    $message = \Swift_Message::newInstance()
+                        ->setSubject($data['subject'])
+                        ->setFrom($data['email'])
+                        ->setTo('mail@alexander-fisher.com')
+                        ->setBody($data['message'])
+                    ;
+                    $this->get('mailer')->send($message);
+                    
+                    return $this->render('FishFrontEndBundle:FrontEnd:contact-success.html.twig');
+                }
+            }
+        }
+        
+        return array(
+            'form' => $form->createView(),
+            'errors' => $errors
+        );
     }
     
     /**
-     * @Route("/code-samples", name="fish_frontend_codesamples")
+     * Route("/code-samples", name="fish_frontend_codesamples")
      * @Template()
      */
     public function codeSamplesAction()
