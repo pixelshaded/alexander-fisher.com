b73309940f52aaa54443c2b30d3933ec6f2040af
Alexander Fisher <afisher@alexander-fisher.com>
2014-06-24 20:15:01 -0400

    Remove php and update readme for branches.

--- src/Fish/FrontEndBundle/FishFrontEndBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Fish\FrontEndBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class FishFrontEndBundle extends Bundle
-{
-}
--- src/Fish/UserBundle/FishUserBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Fish\UserBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class FishUserBundle extends Bundle
-{
-}
