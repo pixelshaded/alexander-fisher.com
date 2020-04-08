4b0e67cbcc80e8e1ac90a3fa3a11a27c081c2e5b
Alexander Fisher <afisher@alexander-fisher.com>
2020-04-07 21:47:29 -0400

    Use stretch and php5.3 so composer install will work. Get composer auth from env.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker rm portfolio
-docker run -i -t --entrypoint /bin/bash --name portfolio afisher-symfony:1.0
+docker run -i -t --entrypoint /bin/bash --name portfolio -e COMPOSER_AUTH="$COMPOSER_AUTH" afisher-symfony:1.0
